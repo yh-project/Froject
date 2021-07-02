d6348ec226d36d356a9e32ac56e1a35a0edf9c08
yh-project <yoha6865@gmail.com>
2021-07-03 02:11:18 +0900

    210702

--- app/src/main/java/com/example/froject/SignupActivity.java
+++ app/src/main/java/com/example/froject/SignupActivity.java
@@ -1,15 +1,19 @@
 package com.example.froject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -70,8 +74,15 @@ public class SignupActivity extends AppCompatActivity {
                                 if (task.isSuccessful()) {
                                     // 회원가입 성공시
                                     FirebaseUser user = mAuth.getCurrentUser();
-                                    startToast("회원가입을 성공했습니다.");
-                                    startActivity(MainActivity.class);
+                                    user.sendEmailVerification()
+                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> task) {
+                                                    if (task.isSuccessful()) {
+                                                        startToast("메일을 전송했습니다.");
+                                                    }
+                                                }
+                                            });
                                 } else {
                                     // 회원가입실패시
                                     if(task.getException() != null) {
