e2f9f72486b608f8d38e701f6bf96710e69b7ad7
Ghanadara <rnjs1522@naver.com>
2021-08-03 16:57:53 +0900

    20210803권영준 제출합니다.

--- app/src/main/java/com/example/froject/Homefragment.java
+++ app/src/main/java/com/example/froject/Homefragment.java
@@ -6,7 +6,10 @@ import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -40,12 +43,6 @@ public class Homefragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_home, container, false);
 
         ImageButton search = v.findViewById(R.id.search);
-        search.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(SearchActivity.class);
-            }
-        });
 
         // 분야사전 배너
         viewpager = v.findViewById(R.id.dictionary_banner);
@@ -56,10 +53,12 @@ public class Homefragment extends Fragment {
         final Runnable Update = new Runnable() {
             @Override
             public void run() {
-                if(currentPage == 5) {
+                if (currentPage == 5) {
                     currentPage = 0;
                 }
                 viewpager.setCurrentItem(currentPage++, true);
+                TextView Dictionary = v.findViewById(R.id.gotoDictionary);
+                Dictionary.setOnClickListener(onClickListener);
             }
         };
 
@@ -74,8 +73,8 @@ public class Homefragment extends Fragment {
         // 카테고리 목록
         categoryView = v.findViewById(R.id.homecategoryRecyclerview);
         categoryList = new ArrayList<>();
-        for(int i=0; i<10; i++) {
-            CategoryData categoryData = new CategoryData(R.drawable.design, "사진·영상"+i);
+        for (int i = 0; i < 10; i++) {
+            CategoryData categoryData = new CategoryData(R.drawable.design, "사진·영상" + i);
             categoryList.add(categoryData);
         }
         categoryAdapter = new CategoryAdapter(categoryList);
@@ -87,22 +86,39 @@ public class Homefragment extends Fragment {
         // 즐겨찾기 목록
         recyclerView = v.findViewById(R.id.likerecyclerview);
         list = new ArrayList<>();
-        for(int i=0; i<10; i++) {
-            LikePostData likePostData = new LikePostData(""+i, ""+i, ""+i );
+        for (int i = 0; i < 10; i++) {
+            LikePostData likePostData = new LikePostData("" + i, "" + i, "" + i);
             list.add(likePostData);
         }
         likelistAdapter = new LikelistAdapter(list);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false));
         recyclerView.setAdapter(likelistAdapter);
 
+        search.setOnClickListener(onClickListener);
         return v;
     }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.search:
+                    startActivity(SearchActivity.class);
+                    break;
+
+                case R.id.gotoDictionary:
+                    startActivity(DictionaryActivity.class);
+                    break;
+            }
+        }
+    };
 
     private void startActivity(Class c) {
         Intent intent = new Intent(getActivity(), c);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
     }
+
 }
 
 
