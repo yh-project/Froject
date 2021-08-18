a8e09d719fd718768288f6fea4f95fa18166d05f
KangChanghan <anycar8515@ynu.ac.kr>
2021-08-19 00:49:36 +0900

    kang 0818
    connect BoardFragment to PostActivity by send Chat button
    //clone cording Chatting

--- app/src/main/java/com/example/froject/PostActivity.java
+++ app/src/main/java/com/example/froject/PostActivity.java
@@ -1,6 +1,9 @@
 package com.example.froject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -11,6 +14,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -29,8 +34,10 @@ public class PostActivity extends AppCompatActivity {
         setContentView(R.layout.activity_post);
 
         PostData postdata = (PostData)getIntent().getSerializableExtra("PostData");
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         TextView titleView = findViewById(R.id.postTitle);
         TextView nameView = findViewById(R.id.userId);
+        Button chatButton = findViewById(R.id.sendChatting);
 
         titleView.setText(postdata.getTitle());
         nameView.setText(postdata.getAuthor());
@@ -45,5 +52,20 @@ public class PostActivity extends AppCompatActivity {
         detailPostAdapter = new DetailPostAdapter(list);
         recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
         recyclerView.setAdapter(detailPostAdapter);
+
+        if (postdata.getEmail().equals(user.getEmail())) {
+            findViewById(R.id.sendChatting).setVisibility(View.INVISIBLE);
+        }
+        else {
+            chatButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(PostActivity.this, ChatActivity.class);
+                    intent.putExtra("you_email", postdata.Email);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }
+            });
+        }
     }
 }
